9e7b1b32e6ee1630997b1c657591ec8f90ec0bbd
Mahe <santos.fd.matheus@gmail.com>
2026-03-30 09:48:18 -0300

    fix: removing future features placeholder

--- task_manager.py
+++ task_manager.py
@@ -1,13 +1,10 @@
-from tasks import dedup_data_task, dedup_run_task, null_task, user_add_task, user_disable_task, user_reset_task, user_copycat_task, mat_validate_task
+from tasks import null_task, user_disable_task, user_reset_task, user_copycat_task
 
 tasks = {
-    "create": user_add_task.execute,
+    #"create": user_add_task.execute,
     "reset": user_reset_task.execute,
     "inactivate": user_disable_task.execute,
     "copycat": user_copycat_task.execute,
-    "dedup_data": dedup_data_task.execute,
-    "dedup_run": dedup_run_task.execute,
-    "mat_validate": mat_validate_task.execute, 
 }
 
 
